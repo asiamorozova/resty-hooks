01d9c603804ee1038c94ba753cd2f484c7fb763b
Anastasia Morozova <morozova.asia@gmail.com>
2020-05-14 21:31:33 -0700

    changed request component with hooks

--- src/components/Header/Request/Request.jsx
+++ src/components/Header/Request/Request.jsx
@@ -1,49 +1,49 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { useResty } from '../../../hooks/Provider';
+
+cosnt Request = () => {
+    const { url, method, body, onChange, onSubmit } = useResty();
+
+    return(
+        <form onSubmit={onSubmit}>
+            <input type="test" name="url" value={url} onChange={onChange} />
+
+            <label>
+                <input type="radio" name="method" value="POST" checked={method === 'POST'} onChange={onChange}/>
+                post
+            </label>
 
 
+            <label>
+                <input type="radio" name="method" value="GET" checked={method === 'GET'} onChange={onChange}/>
+                get
+            </label>
 
 
-const Request = ({ url, method, textBody, onChange, onSubmit }) => (
-  <div>
-    <form onSubmit={onSubmit}>
-      
-      <label> URL: <input type="text" name="url" value={url} onChange={onChange}></input>
-      </label>
-      
-      <div><label> <input type="radio" name="method" value="get"vonChange={onChange}/> GET 
-      </label>
-
-      <label><input type="radio" name="method" value="post"vonChange={onChange}/> POST 
-      </label>
-
-      <label><input type="radio" name="method" value="put" onChange={onChange}/> PUT 
-      </label>
-
-      <label><input type="radio" name="method" value="patch" onChange={onChange}/>PATCH 
-      </label>
-
-      <label><input type="radio" name="method" value="delete" onChange={onChange}/> DELETE 
-      </label>
-      </div>
-
-      <textarea placeholder="JSON Body" name="textBody" value={textBody} onChange={onChange}>
-      </textarea>
-
-      <button type="submit">Submit</button>
-
-    </form>
-  </div>
-
-);
-Request.propTypes = {
-  url: PropTypes.string.isRequired,
-  method: PropTypes.string.isRequired,
-  textBody: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired,
-  onSubmit: PropTypes.func.isRequired
+            <label>
+                <input type="radio" name="method" value="PUT" checked={method === 'PUT'} onChange={onChange}/>
+                put
+            </label>
 
 
+            <label>
+                <input type="radio" name="method" value="PATCH" checked={method === 'PATCH'} onChange={onChange}/>
+                patch
+            </label>
+
+
+            <label>
+                <input type="radio" name="method" value="DELETE" checked={method === 'DELETE'} onChange={onChange}/>
+                delete
+            </label>
+        <textarea name="body" value={body} onChange={onChange}>
+            </textarea>
+            <button>Request</button>
+
+            </form>
+           
+    );
 };
 
 export default Request;
+
